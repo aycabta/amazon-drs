e119581d4e4f952863907ecfd38bfd55d242be19
Code Ass <aycabta@gmail.com>
2016-12-12 20:49:54 +0900

    Add initial tour URL

--- lib/adash/wait_indefinitely.rb
+++ lib/adash/wait_indefinitely.rb
@@ -8,6 +8,11 @@ module Adash
       @code_cv = ConditionVariable.new
       @code_mutex = Mutex.new
       @server = WEBrick::HTTPServer.new({ :BindAddress => '127.0.0.1', :Port => @port })
+      @server.mount_proc('/getting_started', proc { |req, res|
+        res.content_type = 'text/html'
+        content = %Q`<p>Please go <a href="#{amazon_authorization_url('sample device model', 'aaaa00001')}">initial tour</a>.</p>`
+        res.body = "<html><body>\n#{content}\n</body></html>"
+      })
       @server.mount_proc('/', proc { |req, res|
         res.content_type = 'text/html'
         if req.query.include?('code')
@@ -22,6 +27,18 @@ module Adash
         res.body = "<html><body>\n#{content}\n</body></html>"
       })
       @server
+    end
+
+    def amazon_authorization_url(device_model, serial)
+      base = 'https://www.amazon.com/ap/oa?'
+      params = {
+        client_id: 'amzn1.application-oa2-client.b5c87429af104636bd7ae83df68383e1',
+        scope: 'dash:replenish',
+        response_type: 'code',
+        redirect_uri: "http://localhost:#{@port}/",
+        scope_data: %Q`{"dash:replenish":{"device_model":"#{device_model}","serial":"#{serial}","is_test_device":true}}`
+      }
+      "#{base}#{params.map{ |k, v| "#{k}=#{v}" }.join(?&)}"
     end
 
     def get_code
