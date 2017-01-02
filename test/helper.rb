27c0f0276a52298b4af694187a831f0778c04200
Code Ass <aycabta@gmail.com>
2017-01-03 07:13:07 +0900

    Add helper method that is named fixture

--- test/helper.rb
+++ test/helper.rb
@@ -1,5 +1,19 @@
 require 'webmock/test_unit'
 include WebMock::API
 
+def fixture_path
+  File.expand_path('../fixtures', __FILE__)
+end
+
+def fixture(file)
+  data = File.new(File.join(fixture_path, file)).read
+  data.instance_eval do
+    def json
+      @json ||= JSON.parse(self)
+    end
+  end
+  data
+end
+
 WebMock.enable!
 
