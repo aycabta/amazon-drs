9c0d565a343b312a931d76013a446d4af09efed7
Code Ass <aycabta@gmail.com>
2017-01-03 07:40:00 +0900

    Add parse_headers

--- lib/amazon-drs/base.rb
+++ lib/amazon-drs/base.rb
@@ -1,6 +1,31 @@
+require 'time'
+
 module AmazonDrs
   class Base
+    attr_accessor :request_id, :date, :error_type, :error_description_url
+
     def initialize
     end
+
+    def parse_headers(headers)
+      headers.each_pair do |key, value|
+        case key.downcase
+        when 'x-amzn-errortype'
+          # Value examples:
+          #   InvalidTokenException:http://internal.amazon.com/coral/com.amazon.parkeraccioservice/
+          #   OA2InvalidRequestException:http://internal.amazon.com/coral/com.amazon.panda/
+          @error_type, @error_description_url = value.split(':', 2)
+        when 'x-amzn-requestid'
+          # Example:
+          #   X-Amzn-Requestid: d296d296-d1d1-1111-8c8c-0b43820b4382
+          @request_id = value
+        when 'x-amz-date'
+          # Example:
+          #   X-Amz-Date: Mon, 02 Jan 2017 22:35:53 GMT
+          @date = Time.rfc2822(value)
+        end
+      end
+    end
+    private :parse_header
   end
 end
