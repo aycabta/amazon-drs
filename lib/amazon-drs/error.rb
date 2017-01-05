226d4e8a9f22f6f26d7018fab7bf90f0ac294862
Code Ass <aycabta@gmail.com>
2017-01-06 08:08:43 +0900

    Add message attr to Error model

--- lib/amazon-drs/error.rb
+++ lib/amazon-drs/error.rb
@@ -2,6 +2,12 @@ require 'amazon-drs/base'
 
 module AmazonDrs
   class Error < Base
+    attr_accessor :message
+
+    def parse_body
+      json = JSON.parse(body)
+      @message = json['message'] if json['message']
+    end
   end
 end
 
