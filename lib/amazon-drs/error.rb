1bd1fdc3277984df30793690c83ca1eb20f2dab6
Code Ass <aycabta@gmail.com>
2017-01-06 08:10:07 +0900

    Add error and error_description attr to Error model

--- lib/amazon-drs/error.rb
+++ lib/amazon-drs/error.rb
@@ -2,11 +2,13 @@ require 'amazon-drs/base'
 
 module AmazonDrs
   class Error < Base
-    attr_accessor :message
+    attr_accessor :message, :error_description, :error
 
     def parse_body
       json = JSON.parse(body)
       @message = json['message'] if json['message']
+      @error = json['error'] if json['error']
+      @error_description = json['error_description'] if json['error_description']
     end
   end
 end
