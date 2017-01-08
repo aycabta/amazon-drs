1c4878b63f1ff4473ef2bf97d56b5e943f86acc1
Code Ass <aycabta@gmail.com>
2017-01-08 11:10:33 +0900

    Add test for #get_token

--- test/client_test.rb
+++ test/client_test.rb
@@ -29,5 +29,33 @@ class AmazonDrs::Client::Test < Test::Unit::TestCase
       assert_boolean(value)
     end
   end
+  sub_test_case '#get_token' do
+    setup do
+      jaga = fixture('device.json').json['jaga']
+      @drs = AmazonDrs::Client.new(jaga['device_model']) do |c|
+        c.client_id = 'aaa'
+        c.client_secret = 'secret'
+      end
+      access_token_error = fixture('access_token_error.json')
+      stub_request(:post, "https://api.amazon.com/auth/o2/token")
+        .to_return(
+          body: access_token_error,
+          status: 400,
+          headers: {
+            'X-Amzn-Requestid' => 'd296d296-d1d1-1111-8c8c-0b43820b4382',
+            'X-Amz-Date' => 'Mon, 02 Jan 2017 22:35:53 GMT',
+            'X-Amzn-Errortype' => 'OA2InvalidRequestException:http://internal.amazon.com/coral/com.amazon.panda/'
+          }
+        )
+    end
+    test 'invalid token' do
+      ret = @drs.get_token
+      assert_kind_of(AmazonDrs::Error, ret)
+      assert_match(/^[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}$/, ret.request_id)
+      assert_kind_of(Time, ret.date)
+      assert_equal('OA2InvalidRequestException', ret.error_type)
+      assert_equal('http://internal.amazon.com/coral/com.amazon.panda/', ret.error_description_url)
+    end
+  end
 end
 
