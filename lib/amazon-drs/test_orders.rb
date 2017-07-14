7e6133e9872ee4fd0ff1b9ec5254df456b70d98c
Code Ass <aycabta@gmail.com>
2017-07-14 18:29:37 +0900

    Fix TestOrders

--- lib/amazon-drs/test_orders.rb
+++ lib/amazon-drs/test_orders.rb
@@ -1,6 +1,17 @@
+require 'json'
 require 'amazon-drs/base'
 
 module AmazonDrs
   class TestOrders < Base
+    attr_accessor :slot_order_statuses
+    SlotOrderStatus = Struct.new(:order_status, :slot_id)
+
+    def parse_body(body)
+      json = JSON.parse(body)
+      @slot_order_statuses = []
+      json['slotOrderStatuses'].each do |item|
+        @slot_order_statuses << SlotOrderStatus.new(item['orderStatus'], item['slotId'])
+      end
+    end
   end
 end
